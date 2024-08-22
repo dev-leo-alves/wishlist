11d16afcc34c450afa7c95afb3d15ba278334075
leo <leo.alvesoliveira2000@gmail.com>
2024-08-22 18:03:57 -0300

    New components, routes and pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive } from 'vue';
+import Layout from '../Layouts/Layout.vue';
+import TextInput from './Components/TextInput.vue';
+
+defineOptions({layout: Layout});
+
+const form = reactive({
+    url: String,
+});
+
+const submit = () =>{
+    console.log('submit');
+};
+
+</script>
+
+<template>
+    <Head title="| Home" />
+    <h1 class="text-3xl font-bold underline">
+        Wishlist Home
+    </h1>
+
+    <form @submit.prevent="submit">
+        <TextInput name="url" v-model="form.url" :message="form.errors.url" type="url" />
+    </form>
+</template>
+
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         @vite('resources/js/app.js')
+        @vite('resources/css/app.css')
         @inertiaHead
-
+        @routes
 
     </head>
     <body>
